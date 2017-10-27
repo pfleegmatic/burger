18480ab039660ade068d3c3cb75ca127497c44f4
Lee <pflee@nala.roche.com>
2017-10-26 18:21:46 -0700

    css added

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO burgers (burger_name, devoured, date) VALUES ('Son of a Bun', true, CURRENT_TIMESTAMP);
-INSERT INTO burgers (burger_name, devoured, date) VALUES ('Beefshank Redemption', true, CURRENT_TIMESTAMP);
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Son of a Bun', false, CURRENT_TIMESTAMP);
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Beefshank Redemption', false, CURRENT_TIMESTAMP);
 INSERT INTO burgers (burger_name, devoured, date) VALUES ('The Italian Job', false, CURRENT_TIMESTAMP);
 INSERT INTO burgers (burger_name, devoured, date) VALUES ('Moulin Rouge', false, CURRENT_TIMESTAMP);
 INSERT INTO burgers (burger_name, devoured, date) VALUES ('Royale with Cheese', false, CURRENT_TIMESTAMP);
-INSERT INTO burgers (burger_name, devoured, date) VALUES ('The Edible Hulk', true, CURRENT_TIMESTAMP)
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('The Edible Hulk', false, CURRENT_TIMESTAMP)
