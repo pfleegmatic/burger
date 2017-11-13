6bf4b4d3222d2d9077b700d98e7231e156ae80e1
Lee <pflee@nala.roche.com>
2017-11-13 14:56:53 -0800

    heroku 2

--- config/connection.js
+++ config/connection.js
@@ -1,26 +1,26 @@
-// Creation of database connection
-var mysql = require("mysql");
+// // Creation of database connection
+// var mysql = require("mysql");
 
-//SQL CONNECTION
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "Impetus2Chaos#",
-  database: "burger_db"
-});
+// //SQL CONNECTION
+// var connection = mysql.createConnection({
+//   port: 3306,
+//   host: "localhost",
+//   user: "root",
+//   password: "Impetus2Chaos#",
+//   database: "burger_db"
+// });
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+// // Make connection.
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
 
-// Export connection for our ORM to use.
-module.exports = connection;
+// // Export connection for our ORM to use.
+// module.exports = connection;
 
 
 
@@ -42,14 +42,14 @@ connection = mysql.createConnection(process.env.JAWSDB_URL);
  	};
 	
 
-// connection.connect(function(err) {
-// 	if (err) {
-// 		console.error("error conencting: " + err.stack);
-// 		return;
-// 	}
-// 	console.log("connected as id " + connection.threadId);
-// });
+connection.connect(function(err) {
+	if (err) {
+		console.error("error connecting: " + err.stack);
+		return;
+	}
+	console.log("connected as id " + connection.threadId);
+});
 
 // // export the connection back to orm
-connection.connect();
+// connection.connect();
 module.exports = connection;
